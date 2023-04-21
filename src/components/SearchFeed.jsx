f7d0f9812e0c31124bb528c0f3f39d9f4a67da4d
Bentaleb Firas <bentaleb.firas.dev@gmail.com>
2023-04-21 01:03:50 +0100

    Channel Detail component

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
-
+import React, { useState, useEffect } from "react";
+import { Box, Typography } from "@mui/material";
+import { Videos } from "./";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 const SearchFeed = () => {
+  const [videos, setVideos] = useState([]);
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q`)
+    .then((data) =>setVideos(data.items)
+    );
+  }, []);
   return (
-    <div>SearchFeed</div>
-  )
-}
+    <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: "2" }}>
+      <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "#fff" }}>
+        Search Results For: <span style={{ color: "#f31503" }}>Videos</span>
+      </Typography>
+      <Videos videos={videos} />
+    </Box>
+  );
+};
 
-export default SearchFeed
+export default SearchFeed;
