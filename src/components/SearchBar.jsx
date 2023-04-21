d0457dde6f22f1700520ba7c74472b19e4e3b256
Bentaleb Firas <bentaleb.firas.dev@gmail.com>
2023-04-21 01:41:08 +0100

    Search fonctionality added

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,12 +1,21 @@
 import {React,useState} from 'react'
 import { useNavigate } from 'react-router-dom'
 import {Paper,IconButton} from '@mui/material'
-import{Search} from '@mui/icons-material'
+import SearchIcon from '@mui/icons-material/Search';
 const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState('')
+  const navigate = useNavigate()
+  const handleSubmit= (e)=>{
+    e.preventDefault();
+    if(searchTerm){
+      navigate(`/search/${searchTerm}`)
+      searchTerm('')
+    }
+  }
   return (
     <Paper
     component="form"
-    onSubmit={()=>{}}
+    onSubmit={handleSubmit}
     sx={{
         borderRadius:20,
         border:'1px solid #e3e3e3',
@@ -14,9 +23,13 @@ const SearchBar = () => {
         boxShadow:'none',
         mr: {sm:5}
     }}>
-        <input className='search-bar' placeholder='search ....' value="" onChange={ ()=>{}} type="text" />
-        <IconButton type='submit' sx={{p:'10px',color:'red'}}>
-            <Search />
+<input
+        className='search-bar'
+        placeholder='Search...'
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />        <IconButton type='submit' sx={{p:'10px',color:'red'}}>
+            <SearchIcon />
         </IconButton>
     </Paper>
   )
